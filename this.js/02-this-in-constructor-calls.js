cd6bcd5e6b70ae0ea08d06deb678d1e933c4934a
Muhammad-Adil <adil.muhammad93@gmail.com>
2017-12-20 15:48:30 +0500

    this in constructor calls

---
+++ this.js/02-this-in-constructor-calls.js
@@ -0,0 +1,34 @@
+
+
+
+function Person(firstName, LastName){
+	console.log(this);
+	this.firstName = firstName;
+	console.log(this);
+	this.LastName = LastName;
+	console.log(this);
+
+	return this;
+}
+
+const person = new Person("john", "doe"); //constructor call
+console.log(person)
+//output
+//Person{}
+//Person { firstName: 'john'}
+//Person { firstName: 'john', lastName: 'doe' }
+//Person { firstName: 'john', lastName: 'doe' }
+
+
+
+
+
+
+
+
+console.log(person) //undefined
+
+//but
+console.log(global.firstName); //john
+console.log(global.LastName); //doe
+
