616b04ca97b65679fe52a5ad7cc3d4129f05a968
Muhammad-Adil <adil.muhammad93@gmail.com>
2019-02-19 20:21:10 +0500

    Sets initialize

---
+++ JS-Data-Structures-Algorithms/02-Sets.js
@@ -0,0 +1,21 @@
+/* Sets */
+
+function mySet() {
+    // the var collection will hold the set
+    var collection = [];
+    // this method will check for the presence of an element and return true or false
+    this.has = function(element) {
+        return (collection.indexOf(element) !== -1);
+    };
+    // this method will return all the values in the set
+    this.values = function() {
+        return collection;
+    };
+    // this method will add an element to the set
+    this.add = function(element) {
+        if(!this.has(element)){
+            collection.push(element);
+            return true;
+        }
+        return false;
+    };
