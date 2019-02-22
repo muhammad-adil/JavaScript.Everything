c19ea2053559fe08d16b4e6b26862e58e1bc2e98
Muhammad-Adil <adil.muhammad93@gmail.com>
2019-02-22 20:21:10 +0500

    Queue function initialize

---
+++ JS-Data-Structures-Algorithms/03-Queues.js
@@ -0,0 +1,23 @@
+/* Queues */
+
+function Queue () { 
+    collection = [];
+    this.print = function() {
+        console.log(collection);
+    };
+    this.enqueue = function(element) {
+        collection.push(element);
+    };
+    this.dequeue = function() {
+        return collection.shift(); 
+    };
+    this.front = function() {
+        return collection[0];
+    };
+    this.size = function() {
+        return collection.length; 
+    };
+    this.isEmpty = function() {
+        return (collection.length === 0); 
+    };
+}
